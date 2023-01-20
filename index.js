2c994324a13ae4c5b9ef3568d86d1196b11a45c4
Luis Eduardo Cervantes Jaramillo <luis_edu_cj@hotmail.com>
2023-01-20 13:40:59 -0600

    Agregando una ruta y archivo gitignore

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express"
+
+// Creando App
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
     console.log("Servidor corriendo en el puerto 3000")
+})
+
+// Routing
+app.get("/", (req, res) => {
+    res.send("Hola Mundo desde NodeJS")
 })
